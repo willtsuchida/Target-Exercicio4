0485f1a52f9ddb85278e8ba5b87f6910ada5eddb
willtsuchida <willian_hideki@hotmail.com>
2023-03-24 16:42:53 -0300

    Target - Exercicio 4

---
+++ Target-Exercicio4/Program.cs
@@ -0,0 +1,25 @@
+﻿double faturamentoSp = 67836.43;
+double faturamentoRj = 36678.66;
+double faturamentoMg = 29229.88;
+double faturamentoEs = 27165.48;
+double faturamentoOutros = 19849.53;
+
+double faturamentoTotal = faturamentoSp + faturamentoRj + faturamentoMg + faturamentoEs + faturamentoOutros;
+
+Console.WriteLine($"Faturamento total = {faturamentoTotal.ToString("F2")}");
+Console.Write("Faturamento SP: ");
+PercentualFaturamento(faturamentoSp, faturamentoTotal);
+Console.Write("Faturamento RJ: ");
+PercentualFaturamento(faturamentoRj, faturamentoTotal);
+Console.Write("Faturamento MG: ");
+PercentualFaturamento(faturamentoMg, faturamentoTotal);
+Console.Write("Faturamento Es: ");
+PercentualFaturamento(faturamentoEs, faturamentoTotal);
+Console.Write("Faturamento Outros: ");
+PercentualFaturamento(faturamentoOutros, faturamentoTotal);
+
+
+static void PercentualFaturamento(double faturamentoEstado, double faturamentoTotal)
+{
+    Console.WriteLine((faturamentoEstado / faturamentoTotal * 100).ToString("F4") + "%");
+}
